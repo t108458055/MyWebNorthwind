3c05291ab8deddb99de0e187512beae936ec58cb
t108458055 <t108458055@gmail.com>
2023-03-04 21:26:47 +0800

    learning known app-append
    add Tree ViewHelp

---
+++ MyWebNorthwind/Common/DataReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace MyWebNorthwind.Common
+{
+    public static class DataReaderExtensions
+    { // https://www.codemag.com/Article/2207021/Simplifying-ADO.NET-Code-in-.NET-6-Part-1
+        // https://refactoringguru.cn/design-patterns/template-method/csharp/example
+        public static T GetData<T>(this IDataReader dataReader, string content, T returnValue = default)
+        {
+            var value = dataReader[content];
+            if (!value.Equals(DBNull.Value))
+            { 
+                returnValue = (T) value;
+            }
+            return returnValue;
+        }
+
+    }
+}
---
+++ MyWebNorthwind/Infrastructure/TreeViewHelpers.cs
@@ -0,0 +1,87 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+
+public static class TreeViewHelpers
+{
+    private static string GetControllerName(Type controllerType)
+    {
+        var controllerName = controllerType.Name.EndsWith("Controller")
+            ? controllerType.Name.Substring(0,
+            controllerType.Name.Length - "Controller".Length)
+            : controllerType.Name;
+        return controllerName;
+    }
+    private static MethodCallExpression GetMethodCallExpression<TController>(Expression<Action<TController>> actionSelector)
+    {
+        var call = actionSelector.Body as MethodCallExpression;
+        if (call == null)
+        {
+            throw new ArgumentException("You must call a method on " +
+            typeof(TController).Name, "actionSelector");
+        }
+        return call;
+    }
+    // var methodCallExpression = GetMethodCallExpression(action);
+    // var actionName = methodCallExpression.Method.Name;
+    public static string Action<TController>(this IUrlHelper helper,Expression<Action<TController>> action)where TController : Controller
+    {
+        var controllerName = GetControllerName(typeof(TController));
+        var methodCallExpression = GetMethodCallExpression(action);
+        var actionName = methodCallExpression.Method.Name;
+        var routes = RouteValueExtractor.GetRouteValues(methodCallExpression);
+        var link = helper.Action(action: actionName, controller:
+        controllerName, values: routes);
+        return link;
+    }
+}
+
+public class RouteValueExtractor
+{
+    public static object GetRouteValues(MethodCallExpression call)
+    {
+        var routes = new Dictionary<string, object>();
+        var parameters = call.Method.GetParameters();
+        var pairs = call.Arguments.Select((a, i) => new
+        {
+            Argument = a,
+            ParamName = parameters[i].Name
+        });
+        foreach (var item in pairs)
+        {
+            string name = item.ParamName;
+            object value = GetValue(item.Argument);
+            if (value != null)
+            {
+                var valueType = value.GetType();
+                if (valueType.IsValueType)
+                {
+                    routes.Add(name, value);
+                }
+                else
+                {
+                    throw new NotSupportedException("Unsupported parameter type {0}");
+                }
+            }
+        }
+        return DictionaryToObject(routes);
+    }
+    private static object GetValue(Expression expression)
+    {
+        if (expression.NodeType == ExpressionType.Constant)
+        {
+            return ((ConstantExpression)expression).Value;
+        }
+        throw new NotSupportedException("Unsupported parameter expression");
+    }
+    private static dynamic DictionaryToObject(IDictionary<string, object> dictionary)
+    {
+        var expandoObj = new ExpandoObject();
+        var expandoObjCollection = (ICollection<KeyValuePair<string, object>>)expandoObj;
+        foreach (var keyValuePair in dictionary)
+        {
+            expandoObjCollection.Add(keyValuePair);
+        }
+        dynamic eoDynamic = expandoObj;
+        return eoDynamic;
+    }
+}
