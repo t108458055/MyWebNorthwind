834eb3df046587435dfda7d58a016b50f49ac2f4
t108458055 <t108458055@gmail.com>
2023-03-08 20:52:19 +0800

    url from database render to front-end UI

---
+++ MyWebNorthwind/Controllers/NavController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebNorthwind.Controllers
+{
+    public class NavController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("api/getLinks")]
+        [Route("getLinks")]
+        public List<string> GetLinks()
+        {
+            List<string> links = new List<string>();
+            links.Add("/Index");
+            // 在這裡實現從資料庫中讀取超連結的邏輯
+            // 並將它們存儲在 links 列表中
+
+            return links;
+        }
+    }
+}
