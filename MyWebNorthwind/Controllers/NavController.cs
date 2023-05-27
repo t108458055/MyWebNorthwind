fb1a3e0aab38b9b0f7a3b433d74f89f50dc2390f
t108458055 <t108458055@gmail.com>
2023-05-27 17:29:52 +0800

    modity url misstake
    修正 IIS 部屬 ,相對位置url

--- MyWebNorthwind/Controllers/NavController.cs
+++ MyWebNorthwind/Controllers/NavController.cs
@@ -9,12 +9,11 @@ namespace MyWebNorthwind.Controllers
             return View();
         }
 
-        //[Route("api/getLinks")]
-        [Route("getLinks")]
+        [Route("api/getLinks")]        
         public List<string> GetLinks()
         {
             List<string> links = new List<string>();
-            links.Add("/Index");
+            links.Add("/Home/Index");
             // 在這裡實現從資料庫中讀取超連結的邏輯
             // 並將它們存儲在 links 列表中
 
